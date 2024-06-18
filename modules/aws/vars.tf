ff2a7095ea0f29be4029d8470f9494105ba6bc52
kevinmontaldo <132938259+kevinmontaldo@users.noreply.github.com>
2024-06-18 12:19:02 -0300

    Create vars.tf
    Se crea otro vars.tf para declarar las variables y el tipo.
    Este será utilizado por los archivos que se encuentren dentro del directorio AWS.

---
+++ modules/aws/vars.tf
@@ -0,0 +1,24 @@
+variable "vpc-cidr" {
+  type = string  
+}
+
+variable "subnet-A" {
+  type = string
+}
+
+variable "subnet-B" {
+
+  type = string
+}
+
+variable "az-1" {
+  type = string  
+}
+
+variable "az-2" {
+  type = string  
+}
+
+variable "region" {
+  type = string
+}
