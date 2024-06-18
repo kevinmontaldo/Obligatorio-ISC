6607e6b38338d3ff4dded5bc8bbd3794e61142ef
kevinmontaldo <132938259+kevinmontaldo@users.noreply.github.com>
2024-06-18 12:08:42 -0300

    Create vars.tf
    Se pasan las variables y los tipos.

---
+++ vars.tf
@@ -0,0 +1,28 @@
+variable "profile" {
+  type        = string
+}
+
+variable "region" {
+  type        = string
+}
+
+variable "vpc-cidr" {
+  type        = string
+}
+
+variable "subnet-A" {
+
+  type        = string
+}
+
+variable "subnet-B" {
+  type        = string
+}
+
+variable "az-1" {
+  type        = string
+}
+
+variable "az-2" {
+  type        = string
+}
