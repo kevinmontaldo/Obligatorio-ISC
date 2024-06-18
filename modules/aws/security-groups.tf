37ad6468a40377917a1ef4fff68cac028e4fa959
kevinmontaldo <132938259+kevinmontaldo@users.noreply.github.com>
2024-06-18 12:15:27 -0300

    Create security-groups.tf
    Se crea el archivo security-groups.tf
    Se pasan los parámetros básicos para el recurso con sus respectivas reglas de entrada y salida.

---
+++ modules/aws/security-groups.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "sg-obligatorio" {
+  name        = "sg-obligatorio"
+  vpc_id      = aws_vpc.vpc-obligatorio.id
+
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "all"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
